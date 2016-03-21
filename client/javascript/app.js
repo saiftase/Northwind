f18930273ec6f66cc3ba6dafd9a76e782b26ac88
Saif Tase <saiftase@gmail.com>
2016-03-21 01:35:14 -0400

    Displaying items on page

--- client/javascript/app.js
+++ client/javascript/app.js
@@ -1,6 +1,14 @@
 console.log("App.js");
 var app = angular.module('Northwind', []);
 
-app.controller("ListController", function($scope){
+app.controller("ListController", function($http, $scope){
 	console.log("Angular Working");
+
+	$http.get('/api/items/')
+	.then(function (body) {
+		var items = body.data;
+	    $scope.items = items;
+	    return items;
+	}).catch(console.error.bind(console));
+
 })
